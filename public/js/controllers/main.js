ce1799e0f4b1264615e786aa78cfd71a078223f1
tpyoung <tylerparkeyoung@gmail.com>
2016-05-07 19:32:57 -1000

    Dragula Working with events

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -3,52 +3,49 @@
 
 (function() {
 
-  angular.module('kanban')
+var app =  angular.module('kanban')
 
     .controller('MainController', ['$scope', '$http', 'TaskService', 'dragulaService',
       function($scope, $http, TaskService, dragulaService) {
 
-        $scope.tasks = [];
-        TaskService.getTasks().then(function(res) {
-          $scope.tasks = res.data;
+      $scope.tasks = [];
+      TaskService.getTasks().then(function(res) {
+        $scope.tasks = res.data;
+      });
+
+      $scope.addTask = (function (res) {
+      TaskService.addTask(res).then(function(res) {
+        $scope.tasks.push(res.data);
         });
-        $scope.addTask = (function (res) {
-        TaskService.addTask(res).then(function(res) {
-          $scope.tasks.push(res.data);
-          });
+      });
+
+      $scope.editTask = function(id, field, update) {
+      TaskService.editTask(id, field, update).then(function(res) {
+        $scope.tasks = res.data;
         });
+      };
 
-        $scope.editTask = function(id, field, update) {
-        TaskService.editTask(id, field, update).then(function(res) {
-          $scope.tasks = res.data;
-          });
-        };
-
-        $scope.deleteTask = function(id) {
-
-            TaskService.deleteTask(id).then(function(res) {
-             $scope.tasks = res.data;
-            });
-        };
-
-        $scope.$on('first-bag.drag', function (e, el) {
-          el.removeClass('ex-moved');
+      $scope.deleteTask = function(id) {
+      TaskService.deleteTask(id).then(function(res) {
+         $scope.tasks = res.data;
         });
+      };
 
-        $scope.$on('first-bag.drop', function (e, el) {
-          el.addClass('ex-moved');
-        });
+      $scope.$on('first-bag.drag', function (e, el) {
+        console.log('drag');
+      });
 
-        $scope.$on('first-bag.over', function (e, el, container) {
-          container.addClass('ex-over');
-        });
+      $scope.$on('first-bag.drop', function (e, el) {
+        console.log(e);
+        console.log(el);
+      });
 
-        $scope.$on('first-bag.out', function (e, el, container) {
-          container.removeClass('ex-over');
-        });
+      $scope.$on('first-bag.over', function (e, el, container) {
+        console.log('over');
+      });
 
-    dragulaService.options($scope, 'first-bag', {
-      revertOnSpill: true
-    });
+      $scope.$on('first-bag.out', function (e, el, container) {
+        console.log('out');
+      });
   }]);
 }());
