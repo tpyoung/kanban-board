c8fbc710fce298b07dc104f99127a8611e1680ca
ypyang237 <ypyang237@gmail.com>
2016-05-10 11:47:42 -1000

    success redirects back to /tasks

--- public/js/controllers/login.js
+++ public/js/controllers/login.js
@@ -5,15 +5,18 @@
 
   var myApp = angular.module('kanban')
 
-  .controller('LoginController', ['$scope', '$document', '$http', 'LoginService',
-    function($scope, $document, $http, LoginService) {
+  .controller('LoginController', ['$scope', '$document', '$window', '$location','LoginService',
+    function($scope, $document, $window, $location, LoginService) {
 
       $scope.loginUser = (function(res) {
 
-      LoginService.login(res)
-      .then(function(res) {
+      LoginService.login(res).then(function(res) {
         console.log('coming from services - routes - database!!', res.data);
-        //res.data store in local storage
+           if(res.data){
+              // $window.sessionStorage.setItem("user", JSON.stringify(res.data));
+              $scope.user = res.data;
+              $window.location.href="/";
+            }
         });
 
     });
